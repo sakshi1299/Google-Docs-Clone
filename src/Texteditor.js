23652ffa4ac11c001d3dc82c4fcfd1188b4b5f93
sakshi1299 <sakshikshatriya99@gmail.com>
2022-01-02 16:44:39 +0530

    UI set up

---
+++ src/Texteditor.js
@@ -0,0 +1,18 @@
+import React , {useCallback, useEffect , useRef} from 'react';
+import Quill from 'quill';
+import "quill/dist/quill.snow.css";
+export default function Texteditor() {
+    const wrapperRef = useCallback((wrapper) => {
+        if(wrapper == null) return
+
+        wrapper.innerHTML = "";
+        const editor = document.createElement("div")
+        wrapper.append(editor)
+       new Quill(editor , {theme : "snow"})
+
+    }, [])
+    return (
+        <div className="container" ref={wrapperRef}>
+        </div>
+    )
+}
